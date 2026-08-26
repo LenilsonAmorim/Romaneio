e529c6bfa9bd511bc20d92dae7616dd35f77871c
LenilsonAmorim <57912792+LenilsonAmorim@users.noreply.github.com>
2026-08-26 07:03:17 -0300

    Add files via upload

---
+++ Romaneio/Organizador/app_patch.js
@@ -0,0 +1,4 @@
+// Patch lógico
+// Ao salvar, adicionar o endereço à lista atual,
+// reprocessar a rota e incluir na exportação da planilha.
+// Limpar os endereços manuais ao carregar uma nova planilha.
