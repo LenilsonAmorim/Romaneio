ad3794f7c30f9e5479555b11c14d163409087156
LenilsonAmorim <57912792+LenilsonAmorim@users.noreply.github.com>
2026-09-06 20:12:46 -0300

    Excluir app_patch.js

--- Romaneio/Organizador/app_patch.js
+++
@@ -1,4 +0,0 @@
-// Patch lógico
-// Ao salvar, adicionar o endereço à lista atual,
-// reprocessar a rota e incluir na exportação da planilha.
-// Limpar os endereços manuais ao carregar uma nova planilha.
